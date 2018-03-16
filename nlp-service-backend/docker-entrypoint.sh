66dc08510b26cf7c177889b882d420b00edffc55
Simon Hohl <simon.hohl@dainst.org>
2018-03-16 17:03:57 +0100

    Add basic setup for a long running celery task

--- nlp-service-backend/docker-entrypoint.sh
+++ nlp-service-backend/docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-export PYTHONPATH="${NLP_COMPONENTS_ROOT}"
+export PYTHONPATH="${NLP_COMPONENTS_ROOT}:${NLP_SERVICE_ROOT}"
+
+celery worker -A main.celery --loglevel=info > celery.log 2>&1 &
 
 python3 ${NLP_SERVICE_ROOT}/main.py
