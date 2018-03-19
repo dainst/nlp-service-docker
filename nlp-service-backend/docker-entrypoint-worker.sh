538b8fe8f6e56699b75b89a19358e4a7bbfe5080
Simon Hohl <simon.hohl@dainst.org>
2018-03-19 11:51:22 +0100

    Enable live reload for Celery worker code

--- nlp-service-backend/docker-entrypoint-worker.sh
+++ nlp-service-backend/docker-entrypoint-worker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 export PYTHONPATH="${NLP_COMPONENTS_ROOT}:${NLP_SERVICE_ROOT}"
-
-celery -A celery_tasks worker --loglevel=info
+# celery -A celery_tasks worker --loglevel=info # TODO: Start without auto-restart in production setting
+watchmedo auto-restart -- celery -A celery_tasks worker --loglevel=info
