fb7056030e9af05516bb25b34f053ec59bb559e7
Simon Hohl <simon.hohl@dainst.org>
2018-03-20 12:15:53 +0100

    Make celery worker watch for file changes in nlp_components
    The worker should now be restarted when changes to `nlp_components`
    occur. This commit also contains some general refactoring.

--- nlp-service-backend/docker-entrypoint-service.sh
+++ nlp-service-backend/docker-entrypoint-service.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env bash
-
-# TODO: Remove components root, once fully migrated to worker container
-export PYTHONPATH="${NLP_COMPONENTS_ROOT}:${NLP_SERVICE_ROOT}"
-
-python3 ${NLP_SERVICE_ROOT}/main.py
+python3 main.py
--- nlp-service-backend/docker-entrypoint-worker.sh
+++ nlp-service-backend/docker-entrypoint-worker.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
-
-export PYTHONPATH="${NLP_COMPONENTS_ROOT}:${NLP_SERVICE_ROOT}"
-
-# TODO: Add switch for start without auto-restart in production setting
+# TODO: Add environment switch for starting just celery (without watching for file changes) in production setting
 # celery -A celery_tasks worker --loglevel=info
 
-# TODO: Maybe also watch nlp_components for changes?
-watchmedo auto-restart -- celery -A celery_tasks worker --loglevel=info
+watchmedo auto-restart --recursive --patterns="*.py" -- celery -A worker.celery_tasks worker --loglevel=info
