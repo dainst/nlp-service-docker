a5b5190ceeb6a763767a9f8f177617cdc38bf9c8
Simon Hohl <simon.hohl@dainst.org>
2018-03-20 10:35:11 +0100

    Update Docker configuration for new nlp_service directory structure

--- nlp-service-backend/docker-entrypoint-worker.sh
+++ nlp-service-backend/docker-entrypoint-worker.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 export PYTHONPATH="${NLP_COMPONENTS_ROOT}:${NLP_SERVICE_ROOT}"
-# celery -A celery_tasks worker --loglevel=info # TODO: Start without auto-restart in production setting
+
+# TODO: Add switch for start without auto-restart in production setting
+# celery -A celery_tasks worker --loglevel=info
+
+# TODO: Maybe also watch nlp_components for changes?
 watchmedo auto-restart -- celery -A celery_tasks worker --loglevel=info
